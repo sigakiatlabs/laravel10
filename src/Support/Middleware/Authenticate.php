5f46ce08843444bde16a85306e65c505e46b2db5
Lucas Sigaki <lucas.sigaki@luizalabs.com>
2023-02-23 08:26:22 -0300

    chore: ddd

--- src/Support/Middleware/Authenticate.php
+++ src/Support/Middleware/Authenticate.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Middleware;
 
 use Illuminate\Auth\Middleware\Authenticate as Middleware;
 use Illuminate\Http\Request;
--- src/Support/Middleware/ValidateSignature.php
+++ src/Support/Middleware/ValidateSignature.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace App\Http\Middleware;
+namespace Middleware;
 
 use Illuminate\Routing\Middleware\ValidateSignature as Middleware;
 
